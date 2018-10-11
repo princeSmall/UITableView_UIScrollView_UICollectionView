1151e2d61f3add9ca1188abfec15f04b9c9b8ff0
le tong <1263512924@qq.com>
2018-10-11 15:42:29 +0800

    Initial Commit

---
+++ ScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollView
+//
+//  Created by le tong on 2018/10/11.
+//  Copyright © 2018年 le tong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
